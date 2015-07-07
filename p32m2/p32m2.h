af806ee2bbaa05212bc208446beb87ea52170525
totzYuta <yuta.totz@gmail.com>
2015-07-07 15:23:38 +0900

    clone p32m2 repository

---
+++ p32m2/p32m2.h
@@ -0,0 +1,18 @@
+declare p32m2 {
+    output   i_addr<32>;  /* instruction address */
+    input    i_data<32>;  /* instruction data */
+    instrout i_read;      /* instruction read */
+
+    output   d_addr<32>;  /* data address */
+    input    r_data<32>;  /* read data */
+    output   wr_data<32>;  /* write data */
+    output   w_loc<4>;    /* write location */
+    instrout d_read;      /* data read */
+    instrout d_write;     /* data write */
+
+    instrin  reset;       /* reset */
+
+    instr_arg reset();
+}
+
+/* End of file (p32m2.h) */
